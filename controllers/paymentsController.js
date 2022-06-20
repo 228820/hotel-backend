a6e7610a40e330216a121253eb4dace91a3c451d
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-06-20 22:12:28 +0200

    Add days calculation

--- controllers/paymentsController.js
+++ controllers/paymentsController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')('sk_test_51LC2QfBXADR0JfNuwpVkDWWDimjT13925hcFSSzj8umcXfdC4jyvICqtjLR032DTTIVO5SKBOdDguoGArxs2b0A900uE4hBRnj');
 const db = require('../db/pg')
-const APP_DOMAIN = 'http://localhost:3001';
+const dates = require('../utils/dates-counter')
+const APP_DOMAIN = 'http://localhost:3001'
 
 // Payment succeeds 4242 4242 4242 4242
 // Payment requires authentication 4000 0025 0000 3155
@@ -9,7 +10,7 @@ const APP_DOMAIN = 'http://localhost:3001';
 class PaymentsController {
     async getCheckoutSession(req, res) {
         try {
-            const redirect = parseInt(req.params.redirect ?? '0');
+            const redirect = parseInt(req.params.redirect ?? '0')
             const roomId = req.params.room_id
             const reservationId = req.params.reservation_id
             if(!roomId) {
@@ -18,6 +19,9 @@ class PaymentsController {
             if(!reservationId) {
                 return res.status(500).json({ message: 'Incorrect reservation id!' })
             }
+
+            let result = await db.query('SELECT * FROM reservations WHERE reservation_id = $1', [reservationId])
+            const reservation = result.rows[0]
 
             const { rows } = await db.query('SELECT * FROM rooms WHERE room_id = $1', [roomId]);
             if (!rows || !rows.length) {
@@ -32,13 +36,13 @@ class PaymentsController {
                 line_items: [
                     {
                         price_data: {
-                            currency: 'EUR',
+                            currency: 'USD',
                             product_data: {
                                 name: room.title,
                                 description: room.description,
                                 images: [room.img_link]
                             },
-                            unit_amount_decimal: room.price * 100
+                            unit_amount_decimal: room.price * 100 * dates.calculateDaysBetweenDates(reservation.start_date, reservation.end_date)
                         },
                         quantity: 1,
                     },
@@ -49,10 +53,10 @@ class PaymentsController {
             });
 
             if (redirect) {
-                return res.redirect(303, session.url);
+                return res.redirect(303, session.url)
             }
 
-            return res.status(200).json({url: session.url});
+            return res.status(200).json({url: session.url})
         } catch (err) {
             return res.status(500).json({ message: err.message })
         }
@@ -73,18 +77,19 @@ class PaymentsController {
 
         try {
             let result = await db.query('SELECT * FROM reservations WHERE reservation_id = $1', [reservationId])
-            const reservation = result.rows[0];
+            const reservation = result.rows[0]
 
-            result = await db.query('SELECT * FROM rooms WHERE room_id = $1', [reservationId])
-            const room = result.rows[0];
+            result = await db.query('SELECT * FROM rooms WHERE room_id = $1', [roomId])
+            const room = result.rows[0]
 
-            await db.query('INSERT INTO payments (reservation_id, date, amount, card_id) VALUES ($1, $2, $3, $4)', [reservationId, new Date(), room.price, ''])
-            await db.query('UPDATE reservations SET paid = TRUE WHERE reservation_id = $1', [reservationId]);
+            await db.query('INSERT INTO payments (reservation_id, date, amount, card_id) VALUES ($1, $2, $3, $4)',
+            [reservationId, new Date(), room.price * dates.calculateDaysBetweenDates(reservation.start_date, reservation.end_date), ''])
+            await db.query('UPDATE reservations SET paid = TRUE WHERE reservation_id = $1', [reservationId])
         } catch (err) {
-            console.error(err);
+            console.error(err)
         }
 
-        return res.redirect(303, `${APP_DOMAIN}/checkout_success?session_id=${session_id}&reservation_id=${reservationId}&room_id=${roomId}`);
+        return res.redirect(303, `${APP_DOMAIN}/checkout_success?session_id=${session_id}&reservation_id=${reservationId}&room_id=${roomId}`)
     }
 }
 
